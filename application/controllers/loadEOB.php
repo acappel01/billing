394dc9a6cd54a4641a603012612eff16d8cbc3f1
JamesLeB <james@gowanustech.com>
2014-02-11 15:14:07 -0500

    working on file read function

--- application/controllers/loadEOB.php
+++ application/controllers/loadEOB.php
@@ -15,18 +15,75 @@ class LoadEOB extends CI_Controller {
 		$EOB = fread($fh,filesize($file));
 		$a = explode("~",$EOB);
 		$ln = 0;
+		$check{'name'} = 'develope check';
+		$check{'claims'} = array();
 		
-		$line = $a[$ln]; echo "$line<br/>";
 		if($this->getE($a[$ln],0) == 'ISA'){$ln++;}else{$this->badseg($ln);}
+		if($this->getE($a[$ln],0) == 'GS'){$ln++;}else{$this->badseg($ln);}
+		if($this->getE($a[$ln],0) == 'ST'){$ln++;}else{$this->badseg($ln);}
+		if($this->getE($a[$ln],0) == 'BPR'){
+			$check{'date'} = $this->getE($a[$ln],16);
+			$check{'amount'} = $this->getE($a[$ln],2);
+			$ln++;
+		}else{$this->badseg($ln);}
+		if($this->getE($a[$ln],0) == 'TRN'){
+			$check{'number'} = $this->getE($a[$ln],2);
+			$ln++;
+		}else{$this->badseg($ln);}
+		if($this->getE($a[$ln],0) == 'REF'){$ln++;}else{$this->badseg($ln);}
+		if($this->getE($a[$ln],0) == 'REF'){$ln++;}
+		if($this->getE($a[$ln],0) == 'DTM'){$ln++;}else{$this->badseg($ln);}
+		if($this->getE($a[$ln],0) == 'N1'){$ln++;}else{$this->badseg($ln);}
+		if($this->getE($a[$ln],0) == 'N3'){$ln++;}else{$this->badseg($ln);}
+		if($this->getE($a[$ln],0) == 'N4'){$ln++;}else{$this->badseg($ln);}
+		if($this->getE($a[$ln],0) == 'PER'){$ln++;}else{$this->badseg($ln);}
+		if($this->getE($a[$ln],0) == 'N1'){$ln++;}else{$this->badseg($ln);}
+		if($this->getE($a[$ln],0) == 'N4'){$ln++;}else{$this->badseg($ln);}
+		if($this->getE($a[$ln],0) == 'REF'){$ln++;}else{$this->badseg($ln);}
+		if($this->getE($a[$ln],0) == 'LX'){$ln++;}else{$this->badseg($ln);}
+		if($this->getE($a[$ln],0) == 'CLP'){
+			$claim = array();
+			$claim{'echo'} = $this->getE($a[$ln],1);
+			$claim{'tcn'} = $this->getE($a[$ln],7);
+			$claim{'chargeAmount'} = $this->getE($a[$ln],3);
+			$claim{'paidAmount'} = $this->getE($a[$ln],4);
+			$claim{'status'} = $this->getE($a[$ln],2);
 
-		$line = $a[$ln]; echo "$line<br/>";
+			$check{'claims'}[] = $claim;
+			$ln++;
+		}else{$this->badseg($ln);}
+
 		if($this->getE($a[$ln],0) == 'IEA'){$ln++;}else{$this->badseg($ln);}
+		$line = $a[$ln]; echo "$line<br/>";
 
 		#$db1 = $this->load->database('dentrix',true);
 		#$db2 = $this->load->database('dw',true);
 
 		#$json = json_encode($list);
 		#echo "$json";
+		$checkName = $check{'name'};
+		$checkDate = $check{'date'};
+		$checkAmount = $check{'amount'};
+		$checkNumber = $check{'number'};
+		$checkClaims = $check{'claims'};
+		$claim1 = $check{'claims'}[0];
+		$claim1echo = $claim1{'echo'};
+		$claim1tcn = $claim{'tcn'};
+		$claim1chargeAmount = $claim{'chargeAmount'};
+		$claim1paidAmount = $claim{'paidAmount'};
+		$claim1status = $claim{'status'};
+		echo "-------------------<br/>";
+		echo "Check Name: $checkName<br/>";
+		echo "Check Date: $checkDate<br/>";
+		echo "Check Amount: $checkAmount<br/>";
+		echo "Check Number: $checkNumber<br/>";
+		#echo "Check Claims: $checkClaims<br/>";
+		echo "Claim1 Echo: $claim1echo<br/>";
+		echo "Claim1 tcn: $claim1tcn<br/>";
+		echo "Claim1 chargeAmount: $claim1chargeAmount<br/>";
+		echo "Claim1 paidAmount: $claim1paidAmount<br/>";
+		echo "Claim1 status: $claim1status<br/>";
+		echo "-------------------<br/>";
 		echo "load complete...";
 	}
 	function badseg($x){
