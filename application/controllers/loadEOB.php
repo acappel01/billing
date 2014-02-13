8978ee97ad578d21e9e24ffb976f042b399a4ff2
JamesLeB <james@gowanustech.com>
2014-02-13 15:06:58 -0500

    continueing work on file reader, getting ready to upload to data warehouse

--- application/controllers/loadEOB.php
+++ application/controllers/loadEOB.php
@@ -14,39 +14,70 @@ class LoadEOB extends CI_Controller {
 		$fh = fopen($file,'r');
 		$EOB = fread($fh,filesize($file));
 		$a = explode("~",$EOB);
+		$mycheck = $this->readEOBfile($a);
+
+		$number = $mycheck{'number'};
+		$date   = $mycheck{'date'};
+		$myDate = substr($date,0,4) . '-' . substr($date,4,2) . '-' . substr($date,6,2);
+		$amount = $mycheck{'amount'};
+		$status = $mycheck{'status'};
+
+		echo "-------------------<br/>";
+		echo "number: $number<br/>";
+		echo "mydate: $myDate<br/>";
+		echo "amount: $amount<br/>";
+		echo "status: $status<br/>";
+		echo "-------------------<br/>";
+
+		#$db1 = $this->load->database('dentrix',true);
+		$db2 = $this->load->database('dw',true);
+		$rs = $db2->query("select checkNumber from checks where checkNumber = '$number'");
+		$ct = 0; if($rs){ foreach($rs->result_array() as $row){echo "add $ct"; $ct++; } }
+		if($ct == 0){
+			$rs = $db2->query("
+				insert into checks (checkNumber,checkDate,checkAmount,status)
+				values ('$number','$myDate',$amount,'$status')
+			");
+		}
+
+		#$json = json_encode($list);
+		#echo "$json";
+
+	}
+	public function readEOBfile($a){
+		#Function takes EOB file array and converts to EOB object
 		$ln = 0;
-		$check{'name'} = 'develope check';
+		$check{'status'} = 'OK';
 		$check{'claims'} = array();
 		
-		if($this->getE($a[$ln],0) == 'ISA'){$ln++;}else{$this->badseg($ln);}
-		if($this->getE($a[$ln],0) == 'GS'){$ln++;}else{$this->badseg($ln);}
-		if($this->getE($a[$ln],0) == 'ST'){$ln++;}else{$this->badseg($ln);}
+		if($this->getE($a[$ln],0) == 'ISA'){$ln++;}else{$this->badseg($ln); $check{'status'} = 'NO'; $check{'status'} = 'NO';}
+		if($this->getE($a[$ln],0) == 'GS'){$ln++;}else{$this->badseg($ln); $check{'status'} = 'NO';}
+		if($this->getE($a[$ln],0) == 'ST'){$ln++;}else{$this->badseg($ln); $check{'status'} = 'NO';}
 		if($this->getE($a[$ln],0) == 'BPR'){
 			$check{'date'} = $this->getE($a[$ln],16);
 			$check{'amount'} = $this->getE($a[$ln],2);
 			$ln++;
-		}else{$this->badseg($ln);}
+		}else{$this->badseg($ln); $check{'status'} = 'NO';}
 		if($this->getE($a[$ln],0) == 'TRN'){
 			$check{'number'} = $this->getE($a[$ln],2);
 			$ln++;
-		}else{$this->badseg($ln);}
-		if($this->getE($a[$ln],0) == 'REF'){$ln++;}else{$this->badseg($ln);}
+		}else{$this->badseg($ln); $check{'status'} = 'NO';}
+		if($this->getE($a[$ln],0) == 'REF'){$ln++;}else{$this->badseg($ln); $check{'status'} = 'NO';}
 		if($this->getE($a[$ln],0) == 'REF'){$ln++;}
-		if($this->getE($a[$ln],0) == 'DTM'){$ln++;}else{$this->badseg($ln);}
-		if($this->getE($a[$ln],0) == 'N1'){$ln++;}else{$this->badseg($ln);}
-		if($this->getE($a[$ln],0) == 'N3'){$ln++;}else{$this->badseg($ln);}
-		if($this->getE($a[$ln],0) == 'N4'){$ln++;}else{$this->badseg($ln);}
-		if($this->getE($a[$ln],0) == 'PER'){$ln++;}else{$this->badseg($ln);}
-		if($this->getE($a[$ln],0) == 'N1'){$ln++;}else{$this->badseg($ln);}
-		if($this->getE($a[$ln],0) == 'N4'){$ln++;}else{$this->badseg($ln);}
-		if($this->getE($a[$ln],0) == 'REF'){$ln++;}else{$this->badseg($ln);}
-		if($this->getE($a[$ln],0) == 'LX'){$ln++;}else{$this->badseg($ln);}
+		if($this->getE($a[$ln],0) == 'DTM'){$ln++;}else{$this->badseg($ln); $check{'status'} = 'NO';}
+		if($this->getE($a[$ln],0) == 'N1'){$ln++;}else{$this->badseg($ln); $check{'status'} = 'NO';}
+		if($this->getE($a[$ln],0) == 'N3'){$ln++;}else{$this->badseg($ln); $check{'status'} = 'NO';}
+		if($this->getE($a[$ln],0) == 'N4'){$ln++;}else{$this->badseg($ln); $check{'status'} = 'NO';}
+		if($this->getE($a[$ln],0) == 'PER'){$ln++;}else{$this->badseg($ln); $check{'status'} = 'NO';}
+		if($this->getE($a[$ln],0) == 'N1'){$ln++;}else{$this->badseg($ln); $check{'status'} = 'NO';}
+		if($this->getE($a[$ln],0) == 'N4'){$ln++;}else{$this->badseg($ln); $check{'status'} = 'NO';}
+		if($this->getE($a[$ln],0) == 'REF'){$ln++;}else{$this->badseg($ln); $check{'status'} = 'NO';}
+		if($this->getE($a[$ln],0) == 'LX'){$ln++;}else{$this->badseg($ln); $check{'status'} = 'NO';}
 		if($this->getE($a[$ln],0) == 'CLP'){
 			while($this->getE($a[$ln],0) == 'CLP'){
 				$claim = array();
-				# Claim elements that may no be set
-					$claim{'rarCode'} = '';
-					$claim{'rateCode'} = '';
+				$claim{'rarCode'} = ''; # Claim element may no be set
+				$claim{'rateCode'} = '';# Claim element may no be set
 
 				$claim{'echo'} = $this->getE($a[$ln],1);
 				$claim{'tcn'} = $this->getE($a[$ln],7);
@@ -65,7 +96,7 @@ class LoadEOB extends CI_Controller {
 					$claim{'firstName'} = $this->getE($a[$ln],4);
 					$claim{'medicaid'} = $this->getE($a[$ln],9);
 					$ln++;
-				}else{$this->badseg($ln);}
+				}else{$this->badseg($ln); $check{'status'} = 'NO';}
 				while($this->getE($a[$ln],0) == 'NM1'){$ln++;}
 				while($this->getE($a[$ln],0) == 'MIA'){$ln++;}
 				if($this->getE($a[$ln],0) == 'MOA'){
@@ -81,7 +112,7 @@ class LoadEOB extends CI_Controller {
 				if($this->getE($a[$ln],0) == 'DTM'){
 					$claim{'serviceDate'} = $this->getE($a[$ln],2);
 					$ln++;
-				}else{$this->badseg($ln);}
+				}else{$this->badseg($ln); $check{'status'} = 'NO';}
 				while($this->getE($a[$ln],0) == 'DTM'){$ln++;}
 				while($this->getE($a[$ln],0) == 'PER'){$ln++;}
 				while($this->getE($a[$ln],0) == 'AMT'){$ln++;}
@@ -100,14 +131,15 @@ class LoadEOB extends CI_Controller {
 						$claimLine{'apgPercent'} = ""; # may not be set
 						$claimLine{'servRARC'} = ""; # may not be set
 
-						$claimLine{'adaCode'} = explode(":",$this->getE($a[$ln],1));
+						$tempAda = explode(":",$this->getE($a[$ln],1));
+						$claimLine{'adaCode'} = $tempAda[1];
 						$claimLine{'lineCharge'} = $this->getE($a[$ln],2);
 						$claimLine{'linePaid'} = $this->getE($a[$ln],3);
 						$ln++;
 						if($this->getE($a[$ln],0) == 'DTM'){
 							$claimLine{'serviceDate'} = $this->getE($a[$ln],2);
 							$ln++;
-						}else{$this->badseg($ln);}
+						}else{$this->badseg($ln); $check{'status'} = 'NO';}
 						while($this->getE($a[$ln],0) == 'CAS'){
 							if($this->getE($a[$ln],1) == 'PR'){
 								$claimLine{'servAdj'} = $this->getE($a[$ln],2);
@@ -169,39 +201,14 @@ class LoadEOB extends CI_Controller {
 				} # END is SVC loop
 				$check{'claims'}[] = $claim;
 			}
-		}else{$this->badseg($ln);} # END CLP Loop
+		}else{$this->badseg($ln); $check{'status'} = 'NO';} # END CLP Loop
 
-		if($this->getE($a[$ln],0) == 'PLB'){$this->badseg($ln);}
-		if($this->getE($a[$ln],0) == 'SE'){$ln++;}else{$this->badseg($ln);}
-		if($this->getE($a[$ln],0) == 'GE'){$ln++;}else{$this->badseg($ln);}
-		if($this->getE($a[$ln],0) == 'IEA'){$ln++;}else{$this->badseg($ln);}
-
-		#$db1 = $this->load->database('dentrix',true);
-		#$db2 = $this->load->database('dw',true);
-
-		#$json = json_encode($list);
-		#echo "$json";
-
-		echo "-------------------<br/>";
-		$james = array_keys($check{'claims'}[0]);
-		print_r($james);echo "<br/>";
-		$claimList = $check{'claims'};
-		foreach($claimList as $cl){
-			foreach($cl as $cc){
-				echo "$cc :: ";
-			}
-			echo "<br/>";
-			foreach($cl{'claimLines'} as $line){
-				foreach($line as $ele){
-					echo "$ele :: ";
-				}
-				echo "<br/>";
-			}
-			echo "<br/>";
-		}
-		echo "-------------------<br/>";
-
-	}
+		if($this->getE($a[$ln],0) == 'PLB'){$this->badseg($ln); $check{'status'} = 'NO';}
+		if($this->getE($a[$ln],0) == 'SE'){$ln++;}else{$this->badseg($ln); $check{'status'} = 'NO';}
+		if($this->getE($a[$ln],0) == 'GE'){$ln++;}else{$this->badseg($ln); $check{'status'} = 'NO';}
+		if($this->getE($a[$ln],0) == 'IEA'){$ln++;}else{$this->badseg($ln); $check{'status'} = 'NO';}
+		return $check;
+	}#END readEOBfile
 	function badseg($x){
 		echo "bad seg at $x<br/>";
 	}
